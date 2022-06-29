c7d2e8cdadabb7ac7da885a022c5686207869748
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-06-29 18:08:26 +0300

    Added unit of work

---
+++ Meetup.DLL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Meetup.BLL.Interfaces;
+using Meetup.DLL.Contexts;
+
+namespace Meetup.DLL.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly MeetingContext context;
+
+    public UnitOfWork(MeetingContext context)
+    {
+        this.context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public Task<int> Save() => this.context.SaveChangesAsync();
+}
