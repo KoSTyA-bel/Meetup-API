ee32f3ceb159cdfe2c3e4e21809d2b6acb09d8c8
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-06-29 17:53:07 +0300

    Added contexts

---
+++ Meetup.DLL/Contexts/MeetingContext.cs
@@ -0,0 +1,22 @@
+﻿using Meetup.BLL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Meetup.DLL.Contexts;
+
+public class MeetingContext : DbContext
+{
+    public MeetingContext(DbContextOptions<MeetingContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Meeting> Meetings { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Meeting>().HasKey(meeting => meeting.Id);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
